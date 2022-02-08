0c54c281e6e1db3a9ed9d1c9db003568486fb7a2
yilinng <wei9912@gmail.com>
2022-02-08 16:38:36 +0800

    responsibility width

--- app.js
+++ app.js
@@ -11,3 +11,35 @@ for(let i = 0; i < imageClick.length; i++){
   })
 }
 
+//window shop
+
+const windowShopClick = document.querySelector("#borderCheck");
+const checkMark = document.querySelector("#checkMark");
+/// window border click ,border color change
+let borderCheck = false;
+
+windowShopClick.addEventListener('click', function(){
+ 
+ if(borderCheck){
+  document.documentElement.style.cssText = `--border-color: #F0F0F0`;
+  borderCheck = false;
+  checkMark.style.visibility = 'hidden';
+ }else{
+  document.documentElement.style.cssText = `--border-color: #6C63FF`;
+  borderCheck = true;
+  checkMark.style.visibility = 'visible';
+ } 
+})
+
+const heart = document.querySelector("#heart");
+let heartClick = false;
+heart.addEventListener('click', function(){
+  
+  if(heartClick){
+    document.documentElement.style.cssText = `--heart-color: #D1D3D4`;
+    heartClick = false
+  }else{
+    document.documentElement.style.cssText = `--heart-color: #FFB6C1`;
+    heartClick = true
+  }
+})
